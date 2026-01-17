6d4a870f27f9f00297981785331496286ec2dab6
sumedhahire <ahirsumedh40@gmail.com>
2026-01-17 16:21:14 +0000

    fix kernel.ld

--- run.sh
+++ run.sh
@@ -4,6 +4,25 @@ set -xue
 # qemu file path
 QEMU=qemu-system-riscv32
 
+# Path to clang and compiler flags
+CC=clang  # Ubuntu users: use CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
+ 
+ # use c11
+ # 02 use optimiation to generate efficint machine code
+ # -g3 max amount of debug 
+ # -Wall enable major warnings
+ # Wextra enable additional warinings 
+ # compile for 32-bit risv-v
+ # ffreestanding - do not use host libs
+ # fuse-ld=lld use llvm linker
+ # fno-stack-protector disbale unnessary stack protection 
+ #
+
+# Build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
 # OpenSBI firmware (riscv32)
 OPENSBI=/usr/lib/riscv32-linux-gnu/opensbi/generic/fw_dynamic.bin
 
@@ -13,5 +32,6 @@ $QEMU \
   -bios $OPENSBI \
   -nographic \
   -serial mon:stdio \
-  --no-reboot
+  --no-reboot \
+  -kernel kernel.elf
 
